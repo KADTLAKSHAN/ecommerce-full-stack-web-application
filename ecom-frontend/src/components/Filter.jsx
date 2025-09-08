e0cff3c3021564a7a1b8427970f111ea9e985c15
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-09-08 14:30:41 +0530

    adding life to product filter

--- ecom-frontend/src/components/Filter.jsx
+++ ecom-frontend/src/components/Filter.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { FiArrowDown, FiArrowUp, FiRefreshCw, FiSearch } from "react-icons/fi";
 import {
   Button,
   FormControl,
@@ -8,6 +8,7 @@ import {
   Select,
   Tooltip,
 } from "@mui/material";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 
 const Filter = () => {
   const categories = [
@@ -18,10 +19,64 @@ const Filter = () => {
     { categoryId: 5, categoryName: "Toys" },
   ];
 
+  const [searchParams] = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate();
+
   const [category, setCategory] = useState("all");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const currentCategory = searchParams.get("category") || "all";
+    const currentSortOrder = searchParams.get("sortby") || "asc";
+    const currentSearchTerm = searchParams.get("keyword") || "";
+
+    setCategory(currentCategory);
+    setSortOrder(currentSortOrder);
+    setSearchTerm(currentSearchTerm);
+  }, [searchParams]);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (searchTerm) {
+        searchParams.set("keyword", searchTerm);
+      } else {
+        searchParams.delete("keyword");
+      }
+      navigate(`${pathname}?${searchParams.toString()}`);
+    }, 700);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchParams, searchTerm, navigate, pathname]);
 
   const handleCategoryChange = (event) => {
-    setCategory(event.taget.value);
+    const selectedCategory = event.target.value;
+
+    if (selectedCategory === "all") {
+      params.delete("category");
+    } else {
+      params.set("category", selectedCategory);
+    }
+
+    navigate(`${pathname}?${params}`);
+
+    setCategory(event.target.value);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => {
+      const newOrder = prevOrder === "asc" ? "desc" : "asc";
+      params.set("sortby", newOrder);
+      navigate(`${pathname}?${params}`);
+      return newOrder;
+    });
+  };
+
+  const handleClearFilters = () => {
+    navigate({ pathname: window.location.pathname });
   };
 
   return (
@@ -32,6 +87,8 @@ const Filter = () => {
           type="text"
           placeholder="Search Products"
           className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <FiSearch className="absolute left-3 text-slate-800 size={20}" />
       </div>
@@ -66,12 +123,20 @@ const Filter = () => {
             variant="contained"
             color="primary"
             className="flex items-center gap-2 h-10"
+            onClick={toggleSortOrder}
           >
             Sort By
-            <FiArrowUp size={20} />
+            {sortOrder === "asc" ? (
+              <FiArrowUp size={20} />
+            ) : (
+              <FiArrowDown size={20} />
+            )}
           </Button>
         </Tooltip>
-        <button className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none">
+        <button
+          className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none"
+          onClick={handleClearFilters}
+        >
           <FiRefreshCw className="font-semibold" size={16} />
           <span className="font-semibold">Clear Filter</span>
         </button>

a4516e2fb4cc7941a0e96f54d1e7a7ec7bbb4992
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-09-08 15:06:28 +0530

    filter product based on URL params

--- ecom-frontend/src/components/Products.jsx
+++ ecom-frontend/src/components/Products.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchProducts } from "../store/actions";
 import Filter from "./Filter";
+import useProductFilter from "./useProductFilter";
 
 const Products = () => {
   const { isLoading, errorMessage } = useSelector((state) => state.errors);
@@ -11,10 +12,11 @@ const Products = () => {
   const { products } = useSelector((state) => state.products);
 
   const dispatch = useDispatch();
+  useProductFilter();
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchProducts());
+  // }, [dispatch]);
 
   return (
     <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">

e9f0210f81b554b2e92ecff4edd38122e2b3d7a9
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-09-09 13:57:02 +0530

    setting up banner on homepage

---
+++ ecom-frontend/src/utils/constant.js
@@ -0,0 +1,5 @@
+import bannerImageOne from "../assets/sliders/1_banner.png";
+import bannerImageTwo from "../assets/sliders/2_banner.png";
+import bannerImageThree from "../assets/sliders/3_banner.png";
+
+export { bannerImageOne, bannerImageTwo, bannerImageThree };

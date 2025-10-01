858f5ecdbeddb5fb82999b0b8e6e977d050cd34d
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-01 10:17:56 +0530

    create Payment Form in Stripe

---
+++ ecom-frontend/src/components/checkout/PaymentForm.jsx
@@ -0,0 +1,41 @@
+import {
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { useState } from "react";
+import Skeleton from "../shared/Skeleton";
+
+const PaymentForm = ({ clientSecret, totalPrice }) => {
+  const stripe = useStripe();
+  const element = useElements();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (e) => {};
+
+  const paymentElementOption = {
+    layout: "tabs",
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-4">
+      <h2 className="text-xl font-semibold mb-4">Payment Information</h2>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <>
+          {clientSecret && <PaymentElement options={paymentElementOption} />}{" "}
+          {errorMessage && (
+            <div className="text-red-500 mt-2">{errorMessage}</div>
+          )}
+          <button disabled={!stripe || loading}>
+            {!loading ? `Pay $${Number(totalPrice).toFixed(2)}` : "Processing"}
+          </button>
+        </>
+      )}
+    </form>
+  );
+};
+
+export default PaymentForm;

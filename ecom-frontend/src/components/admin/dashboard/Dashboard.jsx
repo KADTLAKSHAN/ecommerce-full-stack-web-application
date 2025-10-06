bc4f1379f5354fd59a226fdfa6cf42b6b11de907
Tharindu Lakshan <167046569+KADTLAKSHAN@users.noreply.github.com>
2025-10-06 11:19:16 +0530

    frontend Development: Building the Interactive Admin Dashboard

--- ecom-frontend/src/components/admin/dashboard/Dashboard.jsx
+++ ecom-frontend/src/components/admin/dashboard/Dashboard.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import DashboardOverview from "./DashboardOverview";
+import { FaBoxOpen, FaDollarSign, FaShoppingCart } from "react-icons/fa";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { productCount, totalRevenue, totalOrders } = {
+    productCount: "8",
+    totalRevenue: "8712.85",
+    totalOrders: "7",
+  };
+
+  return (
+    <div>
+      <div className="flex md:flex-row mt-8 flex-col lg:justify-between border border-slate-400 rounded-lg bg-linear-to-r from-blue-50 to-blue-100 shadow-lg">
+        <DashboardOverview
+          title="Total Products"
+          amount={productCount}
+          Icon={FaBoxOpen}
+        />
+
+        <DashboardOverview
+          title="Total Orders"
+          amount={totalOrders}
+          Icon={FaShoppingCart}
+        />
+
+        <DashboardOverview
+          title="Total Products"
+          amount={totalRevenue}
+          Icon={FaDollarSign}
+          revenue
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
